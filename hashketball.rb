843a5d53258f3a608fbe86995cc6af4b61025442
adventurous-format-6947 <bminczu@gmail.com>
2020-10-15 15:22:27 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -255,7 +255,7 @@ def big_shoe_rebounds
           key.each do |stats, value|
             if stats == :shoe
               if value > size
-                value == size
+                value = size
           
         end
         end
